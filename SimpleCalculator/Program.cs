26fd1fd8536bcab2a664d2195895fdbd15398d14
Ангелина Куличкина <akulichkinaa@mail.ru>
2019-11-11 17:25:38 +0300

    Добавьте файлы проекта.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        {
+            Console.WriteLine("Введите первое число и нажмите клавишу Enter");
+            string str1 = Console.ReadLine();
+
+            Console.WriteLine("Ввведите операцию: +, -, * или : и нажмите клавишу Enter");
+            char operation;
+            operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число и нажмите клавишу Enter");
+            string str2 = Console.ReadLine();
+
+            float value1 = float.Parse(str1);
+            float value2 = float.Parse(str2);
+
+
+            switch (operation)
+            {
+                case '+': Console.WriteLine("Сумма чисел: " + (value1 + value2)); break;
+                case '-': Console.WriteLine("Разность чисел: " + (value1 - value2)); break;
+                case '*': Console.WriteLine("Произведение чисел: " + (value1 * value2)); break;
+                case '/':
+                    if (value2 == 0)
+                        Console.WriteLine("Делить на ноль нельзя");
+                    else
+                        Console.WriteLine("Частное чисел: " + (value1 / value2));
+                    break;
+                default: Console.WriteLine("Неверно"); break;
+            }
+        }
+    }
+}
